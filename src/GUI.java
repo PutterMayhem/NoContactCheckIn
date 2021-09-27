bd779acdf4db5024d64b28f321a27213434e2432
MCMetro <lz0004cb@go.minnstate.edu>
2021-09-27 10:04:15 -0500

    Test of splash page GUI

--- src/GUI.java
+++ src/GUI.java
@@ -2,14 +2,17 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.fxml.FXMLLoader;
 
 public class GUI extends Application {
 
 	@Override
 	public void start(Stage primary) throws Exception {
 		// TODO Auto-generated method stub
-		Group root = new Group();
-		Scene scene = new Scene(root, 500, 500);
+		Parent root = FXMLLoader.load(getClass().getResource("GUI.fxml"));
+		// Group root = new Group();
+		Scene scene = new Scene(root, 750, 500);
 		primary.setTitle("GUI");
 		primary.setScene(scene);
 		primary.show();
