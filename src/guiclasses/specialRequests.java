5daa00ca39e961b15e1de17b52e30bf7628d8104
PutterMayhem <cegustner@gmail.com>
2021-11-07 18:06:50 -0600

    Update specialRequests.java

--- src/guiclasses/specialRequests.java
+++ src/guiclasses/specialRequests.java
@@ -1,10 +1,18 @@
 package guiclasses;
 
-public class specialRequests {
+import javafx.application.Application;
+import javafx.stage.Stage;
 
+public class specialRequests extends Application {
 
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        launch(args);
-    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
 }
