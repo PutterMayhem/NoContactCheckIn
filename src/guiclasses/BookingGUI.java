6ad8f6926185bcf2bc9e81f56d08aeb104597022
mhussein29 <77996250+mhussein29@users.noreply.github.com>
2021-11-09 23:23:36 -0600

    Update BookingGUI.java

--- src/guiclasses/BookingGUI.java
+++ src/guiclasses/BookingGUI.java
@@ -1,9 +1,15 @@
 package guiclasses;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -11,7 +17,7 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-public class BookingGUI extends Application {
+public class BookingGUI extends Application implements Initializable{
 	
 	@FXML
 	private Button submit;
@@ -43,9 +49,22 @@ public class BookingGUI extends Application {
 		}
 	}
 	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		submit.setOnAction(new EventHandler<ActionEvent>( ) {
+			@Override
+			public void handle(ActionEvent event) {
+				
+			}
+		});
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		launch(args);
 	}
+
+	
 
 }
