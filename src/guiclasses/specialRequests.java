9c39236c514afec2a96d4f3fc6fa53742a048a72
MCMetro <lz0004cb@go.minnstate.edu>
2021-11-09 20:17:11 -0600

    Added more formatting, buttons, and text fields.

--- src/guiclasses/specialRequests.java
+++ src/guiclasses/specialRequests.java
@@ -1,7 +1,10 @@
 package guiclasses;
 
 import javafx.application.Application;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.geometry.Side;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -10,12 +13,15 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.text.*;
 import javafx.stage.Stage;
 
 import javax.swing.plaf.ButtonUI;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.time.LocalDate;
+import java.util.Date;
 
 public class specialRequests extends Application {
 
@@ -46,40 +52,163 @@ public class specialRequests extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        //Declare env variables
+        String custEmail = "john.doe@yourdomain.com";
+
         //Declare asset variables.
         String hiltonLogo = "src/images/download2.png";
         String hiltonPhoto = "src/images/hiltonimage2.jpg";
 
-        GridPane gp = new GridPane();
+        //Base Pane for this scene
+        GridPane baseGp = new GridPane();
 
+
+        //////////////////////////////////////////////
+        //Top-Left Pane
+        //////////////////////////////////////////////
+
+        //This pane contains the hotel Logo and customer email. Add to the upper left corner.
         Pane imgPane = new Pane();
+        GridPane logoGrid = new GridPane();
+        Text custEmailTxt = new Text(" " + custEmail);
+        logoGrid.setBackground(new Background(new BackgroundFill(Color.web("#808080"), CornerRadii.EMPTY, Insets.EMPTY)));
+        custEmailTxt.setStyle("-fx-background-color: #808080;");
         ImageView imgView = formatPicture(getAbsolutePath(hiltonLogo));
         imgPane.getChildren().add(imgView);
         imgPane.setBackground(new Background(new BackgroundFill(Color.web("#808080"), CornerRadii.EMPTY, Insets.EMPTY)));
-        gp.add(imgPane, 0, 0);
+        logoGrid.add(custEmailTxt, 0, 0);
+        logoGrid.add(imgPane, 0, 1);
+        baseGp.add(logoGrid, 0, 0);
 
-        // TODO: Add buttons and fields for special requests
+
+        //////////////////////////////////////////////
+        //Top-Middle Pane
+        //////////////////////////////////////////////
+
+        //This pane contains the title of the scene and customer lookup box
+        //Create parent panes
         Pane lookup = new Pane();
         GridPane login = new GridPane();
+        GridPane titleLogin = new GridPane();
+        VBox blankVBox = new VBox();
+
+        //Create text, text box, label, and button elements
+        Text blankText = new Text("");
         Label emailLbl = new Label("Email Address: ");
         TextField emailTxtFld = new TextField("john.doe@yourdomain.com");
         Label confCodeLbl = new Label("Confirmation Code: ");
         TextField confCodeTxtFld = new TextField();
         Button lookupButton = new Button("Lookup");
+        Text title = new Text("Make a Request");
+        title.setFont(Font.font("verdana", FontWeight.NORMAL, FontPosture.REGULAR, 48));
+        blankText.setFont(Font.font("verdana", 48));
 
-        login.add(emailLbl, 0, 0);
-        login.add(emailTxtFld, 1, 0);
-        login.add(confCodeLbl, 2, 0);
-        login.add(confCodeTxtFld, 3, 0);
-        login.add(lookupButton, 4, 0);
+        //Add elements to respective panes for formatting.
+        blankVBox.getChildren().add(blankText);
+        title.setTextAlignment(TextAlignment.CENTER);
+        GridPane.setHalignment(title, HPos.CENTER);
+        GridPane.setHalignment(login, HPos.CENTER);
 
-        lookup.getChildren().add(login);
-        lookup.setMinWidth(825);
+        //Add display panes and elements to the login GridPane
+        titleLogin.add(title, 0, 0);
+        titleLogin.add(blankVBox, 0, 1);
+        titleLogin.add(login, 0,2);
+        login.add(emailLbl, 0, 2);
+        login.add(emailTxtFld, 1, 2);
+        login.add(confCodeLbl, 2, 2);
+        login.add(confCodeTxtFld, 3, 2);
+        login.add(lookupButton, 4, 2);
+
+        //add all elements to the primary top middle pane and display in the base pane.
+        lookup.getChildren().add(titleLogin);
+        lookup.setMinWidth(860);
+        titleLogin.setMinWidth(860);
+
         lookup.setBackground(new Background(new BackgroundFill(Color.web("#808080"), CornerRadii.EMPTY, Insets.EMPTY)));
+        baseGp.add(lookup, 1, 0);
 
-        gp.add(lookup, 1, 0);
 
-        Scene scene = new Scene(gp, 1920, 1080);
+        //////////////////////////////////////////////
+        //Top-Right Pane
+        //////////////////////////////////////////////
+
+        //This is a blank, gray pane
+        Pane blankPaneGray = new Pane();
+        blankPaneGray.setMinWidth(600);
+        blankPaneGray.setBackground(new Background(new BackgroundFill(Color.web("#808080"), CornerRadii.EMPTY, Insets.EMPTY)));
+        baseGp.add(blankPaneGray, 2, 0);
+
+
+        //////////////////////////////////////////////
+        //Middle-Left Pane
+        //////////////////////////////////////////////
+
+        Pane blankPaneWhite = new Pane();
+        blankPaneWhite.setMinWidth(600);
+        baseGp.add(blankPaneWhite, 0, 1);
+
+
+        //////////////////////////////////////////////
+        //Middle Pane
+        //////////////////////////////////////////////
+        LocalDate checkIn = LocalDate.now();
+        LocalDate checkOut = LocalDate.now();
+        String custName = "John Doe";
+        int roomNum = 203;
+        Button  makeReq = new Button("Make a Request");
+
+        Pane custInfo = new Pane();
+        GridPane centerPane = new GridPane();
+        Text custNotice = new Text("Verify the information below is correct,\n then click the 'Make a Request' button.");
+        centerPane.add(custNotice, 0, 0);
+        custInfo.getChildren().add(centerPane);
+
+        GridPane bookingInfo = new GridPane();
+        GridPane hotelInfo = new GridPane();
+        Text custNameTxt = new Text(custName);
+        bookingInfo.add(custNameTxt, 0, 0);
+
+        Label checkInLbl = new Label("Check in: ");
+        Label checkOutLbl = new Label("Check out: ");
+        Label rmNum = new Label("Room #: ");
+        Text checkInTxt = new Text(String.valueOf(checkIn));
+        Text checkOutTxt = new Text(String.valueOf(checkOut));
+        Text roomNumTxt = new Text(String.valueOf(roomNum));
+        hotelInfo.add(blankText, 0,0);
+        hotelInfo.add(checkInLbl, 0,1);
+        hotelInfo.add(checkInTxt,1,1);
+        hotelInfo.add(checkOutLbl,0,2);
+        hotelInfo.add(checkOutTxt, 1,2);
+        hotelInfo.add(rmNum, 2, 1);
+        hotelInfo.add(roomNumTxt,3,1);
+        bookingInfo.add(hotelInfo, 0, 2);
+        bookingInfo.add(makeReq, 0, 3);
+        centerPane.add(bookingInfo, 0, 1);
+        GridPane.setHalignment(makeReq, HPos.RIGHT);
+        GridPane.setHalignment(custNotice, HPos.CENTER);
+        GridPane.setHalignment(custNameTxt, HPos.CENTER);
+
+        custInfo.setMinWidth(860);
+        bookingInfo.setMinWidth(860);
+        hotelInfo.setMinWidth(860);
+        baseGp.add(custInfo, 1,1);
+
+
+
+        Pane hiltonBg = new Pane();
+//        hiltonBg.setBackground(
+//                new Background(
+//                    new BackgroundImage(
+//                        new Image(String.valueOf(getAbsolutePath(hiltonPhoto))),
+//                        BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT,
+//                        new BackgroundPosition(Side.LEFT, 0, true, Side.BOTTOM, 0, true),
+//                        new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, true, true, false, true)
+//                    )
+//                )
+//        );
+        baseGp.add(hiltonBg, 0, 1);
+
+        Scene scene = new Scene(baseGp, 1920, 1080);
 
         stage.setTitle("UCheckIn - Make a Request");
         stage.setScene(scene);
